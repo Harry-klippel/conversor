0030d8a80b297da98ba5870c2fb9861355f7396c
Harrison L. Klippel <108628473+Harry-klippel@users.noreply.github.com>
2022-10-17 18:51:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const valorConvertido = document.querySelector('#valorConvertido')
+const buttom = document.querySelector('#buttom')
+buttom.onclick = botaoCarregar
+
+ function transJson(response){
+    return response.json() 
+}
+
+async function botaoCarregar(){
+
+    const dados = await fetch(`https://v6.exchangerate-api.com/v6/290988fdcfedb027156fa5a3/latest/USD`)
+
+      .then(transJson)
+      .then((exibir) => {
+        //console.log(exibir)
+        const inputOne = document.querySelector('#valor')
+        const valor = inputOne.value
+        const input = parseFloat(valor
+            )
+        const rate = exibir.conversion_rates.BRL
+        const cal = (rate * input)
+        valorConvertido.innerText = `R$ ${cal.toFixed(2)}`
+        console.log(cal)
+    
+    })
+      .catch('')
+
+}
+
+
+
+
+
+
+
+
+
